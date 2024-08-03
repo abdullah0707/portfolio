26ee6a60d360c9a56b81a3846f6babdddcd81c77
abdullah0707 <abdullahelgammal25@gmail.com>
2024-08-03 21:12:12 +0300

    up 20

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,99 +1,99 @@
 <template>
   <div class="custom-section h-screen overflow-scroll">
-      <h1 class="text-center font-semibold text-5xl py-7 text-white">Contact us</h1>
-      <div class="flex flex-col h-100 justify-center px-6 lg:px-8 ">
-        <div class="lg:mt-10 mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form class="space-y-6 text-sm mx-2" action="#" method="POST" @submit.prevent="SubmitForm(form)">
-            <div>
-              <label for="name" class="block">
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                  Name
-                </span>
-                <input type="name" name="name" id="name" v-model="form.name" required
-                  class="mt-1 px-3 py-2  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-1"
-                  placeholder="Your Name" />
-              </label>
-            </div>
-            <div>
-              <label for="email" class="block">
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                  Email
-                </span>
-                <input type="email" name="email" id="email" v-model="form.email" required
-                  class="peer mt-1 px-3 py-2  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-1"
-                  placeholder="you@example.com" />
-              </label>
-            </div>
-            <div>
-              <label for="phone" class="block text-sm font-semibold leading-6 text-white">
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                  Phone number
-                </span>
-                <div class="relative mt-2.5">
-                  <div class="absolute inset-y-0 left-0 flex items-center">
-                    <label for="country" class="sr-only">Country</label>
-                    <select id="country" name="country"
-                      class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-2 text-gray-400 focus:outline-none sm:text-sm"
-                      v-model="form.country">
-                      <option>EG</option>
-                      <option>AS</option>
-                      <option>US</option>
-                    </select>
-                  </div>
-                  <input type="number" name="phone" id="phone" autocomplete="phone" v-model="form.phone" required
-                    class="block w-full px-3.5 py-2 pl-20 text-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 rounded-md sm:text-sm focus:ring-1 sm:leading-6"
-                    placeholder="Your Number">
+    <h1 class="text-center font-semibold text-white text-6xl pt-10 pb-20">Contact me</h1>
+    <div class="flex flex-col h-100 justify-center px-6 lg:px-8 ">
+      <div class="lg:mt-10 mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6 text-sm mx-2" action="#" method="POST" @submit.prevent="SubmitForm(form)">
+          <div>
+            <label for="name" class="block">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+                Name
+              </span>
+              <input type="name" name="name" id="name" v-model="form.name" required
+                class="mt-1 px-3 py-2  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="Your Name" />
+            </label>
+          </div>
+          <div>
+            <label for="email" class="block">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+                Email
+              </span>
+              <input type="email" name="email" id="email" v-model="form.email" required
+                class="peer mt-1 px-3 py-2  border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-1"
+                placeholder="you@example.com" />
+            </label>
+          </div>
+          <div>
+            <label for="phone" class="block text-sm font-semibold leading-6 text-white">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+                Phone number
+              </span>
+              <div class="relative mt-2.5">
+                <div class="absolute inset-y-0 left-0 flex items-center">
+                  <label for="country" class="sr-only">Country</label>
+                  <select id="country" name="country"
+                    class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-2 text-gray-400 focus:outline-none sm:text-sm"
+                    v-model="form.country">
+                    <option>EG</option>
+                    <option>AS</option>
+                    <option>US</option>
+                  </select>
                 </div>
-              </label>
-            </div>
-            <div>
-              <label for="subject" class="block">
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                  Subject
-                </span>
-                <input type="text" name="subject" id="subject" v-model="form.subject" required
-                  class="mt-1 px-3 py-2   border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-5"
-                  placeholder="Topic title" />
-              </label>
-            </div>
+                <input type="number" name="phone" id="phone" autocomplete="phone" v-model="form.phone" required
+                  class="block w-full px-3.5 py-2 pl-20 text-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 rounded-md sm:text-sm focus:ring-1 sm:leading-6"
+                  placeholder="Your Number">
+              </div>
+            </label>
+          </div>
+          <div>
+            <label for="subject" class="block">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+                Subject
+              </span>
+              <input type="text" name="subject" id="subject" v-model="form.subject" required
+                class="mt-1 px-3 py-2   border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-5"
+                placeholder="Topic title" />
+            </label>
+          </div>
 
-            <div>
-              <label for="message" class="block">
-                <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
-                  Message
-                </span>
-                <textarea id="message" name="message" v-model="form.message" type="textarea" required
-                  class="mt-1 px-3 py-2   border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-5"
-                  placeholder="Your letter" />
-              </label>
-            </div>
+          <div>
+            <label for="message" class="block">
+              <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-white">
+                Message
+              </span>
+              <textarea id="message" name="message" v-model="form.message" type="textarea" required
+                class="mt-1 px-3 py-2   border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-yellow-400 focus:ring-yellow-400 block w-full rounded-md sm:text-sm focus:ring-5"
+                placeholder="Your letter" />
+            </label>
+          </div>
 
-            <div>
-              <button type="submit"
-                class="flex w-full justify-center rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-gray-600">
-                <template v-if="waiting">
-                  <i class="fa-solid fa-spinner fa-spin-pulse p-1"></i>
-                  Loading...
-                </template>
-                <template v-if="!waiting">
-                  Sand Your Message
-                </template>
-              </button>
-            </div>
-          </form>
-        </div>
-        <div class="my-4 mx-auto w-full">
-          <UNotification v-if="succsess" icon="i-heroicons-check-circle" color="primary" :id="3"
-            title="Your message was sent successfully!"
-            description="Thank you for sending. Your message has been received. I will contact you as soon as possible."
-            :close-button="null" :callback="onCallback()" :timeout="timer" />
-        </div>
-        <div class="my-4 mx-auto w-full">
-          <UNotification v-if="errors" label="Show toast" icon="i-heroicons-x-circle" color="red" :id="6"
-            title="Your message was not sent!"
-            description="I am sorry that your message did not reach me. Please try sending it again." :timeout="timer"
-            :callback="onCallback()" :close-button="null" />
-        </div>
+          <div>
+            <button type="submit"
+              class="flex w-full justify-center rounded-md bg-gray-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:bg-gray-600">
+              <template v-if="waiting">
+                <i class="fa-solid fa-spinner fa-spin-pulse p-1"></i>
+                Loading...
+              </template>
+              <template v-if="!waiting">
+                Sand Your Message
+              </template>
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="my-4 mx-auto w-full">
+        <UNotification v-if="succsess" icon="i-heroicons-check-circle" color="primary" :id="3"
+          title="Your message was sent successfully!"
+          description="Thank you for sending. Your message has been received. I will contact you as soon as possible."
+          :close-button="null" :callback="onCallback()" :timeout="timer" />
+      </div>
+      <div class="my-4 mx-auto w-full">
+        <UNotification v-if="errors" label="Show toast" icon="i-heroicons-x-circle" color="red" :id="6"
+          title="Your message was not sent!"
+          description="I am sorry that your message did not reach me. Please try sending it again." :timeout="timer"
+          :callback="onCallback()" :close-button="null" />
+      </div>
     </div>
   </div>
 </template>
